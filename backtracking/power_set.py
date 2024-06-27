b1e00e23832f10cd1c5fac542932c3e1259f577f
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-06-27 10:00:12 -0700

    Solve finding a subset with duplication using backtracking

--- backtracking/power_set.py
+++ backtracking/power_set.py
@@ -16,3 +16,38 @@ def power_set(nums):
         subset.pop()
     helper(0, nums, [])
     return output
+
+
+# psuedo code
+# if i == length, push
+#     to result
+# # exclude
+# helper(i+1, nums, subset)
+
+# # include
+# push nums[i] to subset
+# helper(i+1, nums, subset)
+# pop last element from subset
+
+
+# Time complexity = O(2^n * n)
+# Space complexity = O(n)
+def power_set_with_duplication(nums):
+    output = []
+    def helper(index, subset):
+        if index == len(nums):
+            output.append(subset.copy())
+            return
+        
+        # Include part
+        subset.append(nums[index])
+        helper(index + 1, subset)
+        subset.pop()
+
+        # Exclusive part
+        while(index < len(nums) -1 and nums[index] == nums[index+1]):
+            index += 1
+        helper(index + 1, subset)
+    
+    helper(0, [])
+    return output
