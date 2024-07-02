820d41301c8c1d9b1d0d3ab165b55a4dea6485fe
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-02 12:03:34 -0700

    Solve sudoku problem using backtracking

--- backtracking/sudoku.py
+++ backtracking/sudoku.py
@@ -1,31 +1,40 @@
-board = [
-["5", "3", ".", ".", "7", ".", ".", ".", "."],
-["6", ".", ".", "1", "9", "5", ".", ".", "."],
-[".", "9", "8", ".", ".", ".", ".", "6", "."],
-["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-[".", "6", ".", ".", ".", ".", "2", "8", "."],
-[".", ".", ".", "4", "1", "9", ".", ".", "5"],
-[".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+# Time Complexity = O(1)
+# 9 ^ 81 = 9 ^ maximum empty cells
+# Space Complexity = O(1)
+# 9 * 9 = n * n
 
-def condition_checker(i, j, num, board):
-        # Check row
-        if num in board[i]:
-            return True
-        # Check column
-        for row in range(9):
-            if num == board[row][j]:
-                return True
-        # Check grid
-        grid_row = i // 3
-        grid_column = j // 3
-        for row in range(grid_row * 3, grid_row * 3 + 3):
-            for column in range(grid_column * 3, grid_column * 3 + 3):
-                if num == board[row][column]:
-                    return True
-        return False
+def solveSudoku(board):
+    SUDOKU_SIZE = 9
+
+    def helper():
+        for row in range(SUDOKU_SIZE):
+            for column in range(SUDOKU_SIZE):
+                if board[row][column] == ".":
+                    for num in '123456789':
+                        if is_valid(row, column, num):
+                            board[row][column] = num
+                            if helper(): return True
+                            board[row][column] = "."
+                    return False
+        return True
                     
-print(condition_checker(7, 2, "6", board))
-
-
+                
+    def is_valid(row, column, num):
+        for index in range(SUDOKU_SIZE):
+            # Column check
+            if num == board[index][column]:
+                return False
+            
+            # Row check
+            if num == board[row][index]:
+                return False
+            
+            # Grid check
+            curr_row = (row // 3) * 3 + index // 3
+            curr_col = (column // 3) * 3 + index % 3
+            if num == board[curr_row][curr_col]:
+                return False
+                
+            return True
+    helper()
+    
