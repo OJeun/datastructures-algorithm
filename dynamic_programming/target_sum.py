1b31f56f71b17d4772f6be90fe110ab3fc02a66f
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-10 11:27:28 -0700

    Improve time complexity by using memoization

--- dynamic_programming/target_sum.py
+++ dynamic_programming/target_sum.py
@@ -47,6 +47,24 @@ def findTargetSumWays(nums, target):
     
     return helper(0, 0)
 
+def findTargetSumWays_with_memoization(nums, target):
+    n = len(nums)
+    summation = sum(nums)
+    dp = [[None]*(2*summation+1) for _ in range(n)]
+
+    def helper(index,sum_nums):
+        #base case
+        if index<0:
+            if sum_nums==target:return 1
+            else:return 0
+        if dp[index][sum_nums+summation]!=None:return dp[index][sum_nums+summation]
+
+        positive = helper(index-1,sum_nums + nums[index])    
+        negative = helper(index-1,sum_nums + -1*nums[index])
+
+        dp[index][sum_nums + summation] = negative+positive
+        return dp[index][sum_nums+summation]
+    return helper(n-1,0) 
 
 
 
