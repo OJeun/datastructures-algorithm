4d34341150f03fc2fe672ee4b209fce79db8b370
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-12 10:31:29 -0700

    Improve space complexity by using tabualation

--- dynamic_programming/partition_equal_subset_sum.py
+++ dynamic_programming/partition_equal_subset_sum.py
@@ -32,7 +32,7 @@ def can_partition_memoization(nums):
         return False
         
     target = total // 2
-    dp = [[None]*(2 * target + 1) for _ in range(length_of_nums)]
+    dp = [[None]*(target + 1) for _ in range(length_of_nums)]
     
     def helper(index, curr_sum):
         if curr_sum == target:
@@ -42,7 +42,7 @@ def can_partition_memoization(nums):
             return False
             
         if dp[index][target+curr_sum] != None:
-            return dp[index][target+curr_sum]
+            return dp[index][curr_sum]
     
         # Include
         include = helper(index + 1, curr_sum + nums[index])
@@ -50,7 +50,33 @@ def can_partition_memoization(nums):
         # Exclude
         exclude = helper(index + 1, curr_sum)
         
-        dp[index][target + curr_sum] = include or exclude
-        return dp[index][target + curr_sum]
+        dp[index][curr_sum] = include or exclude
+        return dp[index][curr_sum]
     
     return helper(0, 0)
+
+# nums = [1, 5, 11, 5]
+def can_partition_with_tabulation_sol(nums):
+    n = len(nums)
+    sum = sum(nums)
+
+    if sum % 2 != 0: return False
+    target =sum//2
+    prev = [False] * (target + 1) 
+    curr = [False] * (target + 1)
+    prev[0] = True
+    curr[0] = True
+
+    for i in range(1, n + 1):
+        for j in range(1, target + 1):
+            #pick
+            if nums[i-1] <= j:
+                # by adding nums[i-1] on prev[j-nums], can achieve curr[j]
+                curr[j] = prev[j-nums[i-1]]
+            else:
+                curr[j] = False    
+            #dontpick
+            curr[j] = curr[j] or prev[j]
+            
+        prev = curr[:]  
+    return curr[target] 
