3e304bffdb44c79a14021dfb29830d6db519e5c5
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-15 10:58:48 -0700

    Use tabulation approach to improve space complexity

--- dynamic_programming/longest_common_subsequene.py
+++ dynamic_programming/longest_common_subsequene.py
@@ -1,6 +1,6 @@
 # Time complexity = O(2^(n + m))
 # Space Complexity = O(n + m)
-def longestCommonSubsequence(text1, text2):
+def longest_common_subsequence(text1, text2):
     length_t1 = len(text1)
     length_t2 = len(text2)
     
@@ -25,7 +25,7 @@ def longestCommonSubsequence(text1, text2):
 
 # Time complexity = O(n+m)
 # Space Complexity = O(n + m) + O(n*m)
-def longestCommonSubsequence_memoization(text1, text2):
+def longest_common_subsequence_memoization(text1, text2):
     length_t1 = len(text1)
     length_t2 = len(text2)
     dp = [[None] * length_t1 for _ in range(length_t2)]
@@ -54,4 +54,41 @@ def longestCommonSubsequence_memoization(text1, text2):
         
     return helper(0, 0)
 
-print(longestCommonSubsequence_memoization(text1 = "pbcdq", text2 = "pcq"))
+def longest_common_subsequence_tabulation(text1, text2):
+    length_t1 = len(text1) # pbcdq
+    length_t2 = len(text2) # pcq
+ 
+    prev = [0] * length_t2
+    curr = [0] * length_t2
+    count = 0
+
+    for index1 in range(length_t1):
+        for index2 in range(length_t2):
+            if text1[index1] == text2[index2]:
+                count += 1
+
+    return count
+
+# Time Complexity = O(n * m)
+# Space Complexity = O(n * m)
+def longest_common_subsequence_tabulation_2(text1, text2):
+    length_t1 = len(text1) # pbcdq
+    length_t2 = len(text2) # pcq
+ 
+    dp = [[0] * (length_t1 + 1) for _ in range(length_t2+1)]
+    print(dp)
+
+    for index1 in range(1, length_t1 + 1):
+        for index2 in range(1, length_t2 + 1):
+            if text1[index1 - 1] == text2[index2 - 1]:
+                dp[index2][index1] = dp[index2 - 1][index1 - 1] + 1
+            else: 
+                left = dp[index2 - 1][index1]
+                top = dp[index2][index1 - 1]
+                dp[index2][index1] = max(left, top)
+    return dp[length_t2][length_t1]
+
+
+
+
+print(longest_common_subsequence_tabulation_2(text1 = "pbcdq", text2 = "pcq"))
