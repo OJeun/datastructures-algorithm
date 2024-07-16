c74906739215b08e38f4bc3ba831561ffc059a47
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-16 12:27:14 -0700

    Improve space complexity by opmizing tabulation

--- dynamic_programming/edit_distance.py
+++ dynamic_programming/edit_distance.py
@@ -51,10 +51,59 @@ def edit_distance_memoization(text1, text2):
         
         # Not Equal
         insert = 1 + helper(index1, index2+1)
-        delete = 1 + helper(index1, index2)
+        delete = 1 + helper(index1+1, index2)
         replace = 1 + helper(index1+1, index2+1)
 
         dp[index1][index2] = min(insert, delete, replace)
         return dp[index1][index2]
     
     return helper(0,0)
+
+# Space Complexity = O(n * m)
+# Time Complexity = O(n * m)
+def edit_distance_tabulation(text1, text2):
+    len_1 = len(text1)
+    len_2 = len(text2)
+    dp = [[-1] * (len_1 + 1) for _  in range(len_2+1)]
+    
+    for j in range(len_2+1):
+        dp[0][j] = j
+    
+    for j in range(len_1+1):
+        dp[j][0] = j
+
+    for row in range(1, len_2 + 1):
+        for col in range(1, len_1 + 1):
+            if text2[row] == text1[col]:
+                dp[row][col] = dp[row-1][col-1]
+            else:
+                top = 1 + dp[row-1][col]
+                left = 1 + dp[row][col-1]
+                diagonal = 1 + dp[row-1][col-1]
+                dp[row][col] = min(top, left, diagonal)
+
+    return dp[len_2][len_1]
+
+# Time complexity = O(m*n)
+# Space Complexity = O(m) or O(n)
+def edit_distance_optimized_tabulation(text1, text2):
+    len_1 = len(text1)
+    len_2 = len(text2)
+    prev = [0] * len_1
+    curr = [0] * len_1
+
+    for j in range(len_2+1):
+        prev[j] = j
+
+    for row in range(1, len_2+1):
+        curr[0] = row
+        for col in range(1, len_1+1):
+            if text1[col-1] == text2[row-1]:
+                curr[col] = prev[col-1]
+            else:
+                replace = 1 + prev[col-1]
+                delete = 1 + prev[col]
+                insert = 1 + curr[col-1]
+                curr[col] = min(replace, delete, insert)
+        prev = curr[:]
+    return curr[len_1]
