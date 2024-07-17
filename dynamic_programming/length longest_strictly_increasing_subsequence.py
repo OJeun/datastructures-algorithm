1a5b7bf30b979ddeab83991a7a70a55f84d6b7af
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-17 11:36:42 -0700

    Solve LIS with memoization to improve time complexity

--- dynamic_programming/length longest_strictly_increasing_subsequence.py
+++ dynamic_programming/length longest_strictly_increasing_subsequence.py
@@ -16,3 +16,28 @@ def length_of_LIS(nums):
         return max(exclude, include)
     
     return helper(0, -1)
+
+# Time Complexity = O(n^2)
+# Space Complexity = O(n^2)
+def length_of_LIS_memoization(nums):
+    length_num = len(nums)
+
+    dp = [[-1] * length_num for _ in range(length_num)]
+
+    def helper(curr, prev):
+        if curr > length_num - 1:
+            return 0
+        
+        if dp[prev+1][curr] != -1:
+            return dp[prev+1][curr] 
+
+        exclude = helper(curr + 1, prev)
+
+        include = 0
+        if prev == -1 or nums[curr] > nums[prev]:
+            include = 1 + helper(curr + 1, curr)
+    
+        dp[prev+1][curr] = max(exclude, include)
+        return dp[prev+1][curr] 
+    
+    return helper(0, -1)
