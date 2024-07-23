675b640360d9cb2312e425d9c009d7ae81def531
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-23 11:49:23 -0700

    Solve palindrome problem with tabulation approach

--- dynamic_programming/palindrome.py
+++ dynamic_programming/palindrome.py
@@ -1,31 +1,47 @@
+# Time complexity = O(n^2)
+# Space Complexity = O(n^2)
 def countSubstrings(s):
-    n = len(s)
-    dp = [[-1]*n for _ in range(n)]
+    n = len(s) # n = 6
+    dp = [[-1] * n for _ in range(n)]
+    count = 0
+    
+    for i in range(n):
+        dp[i][i] = True # lenght = 1 -> true 
+        count += 1
+        
+    for row in range(n - 2, -1, -1): # 4 to 0
+        for col in range(row + 1, n):  
+            if s[row] == s[col] and (col == row + 1 or dp[row+1][col-1]):
+                dp[row][col] = True
+                count += 1
+            else:
+                dp[row][col] = False
+    return count
 
-    def helper(i,j):
-        if i==j:
-            dp[i][j]=True
-            return dp[i][j]
+print(countSubstrings("pqprps"))
 
-        if dp[i][j]!= -1:
-            return dp[i][j]    
-        helper(i+1,j)
-        helper(i,j-1)
-        if s[i]==s[j] and (j==i+1 or helper(i+1,j-1)):
-            dp[i][j] = True
+def countSubstrings(s):
+    n = len(s) # n = 6
+    dp = [[-1] * n for _ in range(n)]
+    count = 0
+    
+    for i in range(n):
+        dp[i][i] = True # lenght = 1 -> true 
+        count += 1
+
+    for i in range(n-1): # 0 to 4
+        if s[i] == s[i+1]:
+            dp[i][i+1] = True
+            count += 1
         else:
-            dp[i][j] = False   
-        return dp[i][j]     
-    helper(0,n-1)
+            dp[i][i+1] = False
 
-    #count the number of times we have True in dp
-    res = 0
-    for l in range(1,n+1):
-        for i in range(n-l+1):
-            j = i+l - 1   
-            if dp[i][j] ==True:
-                res+=1
-    return res            
-
-s = "v"
-print(countSubstrings(s))
+    for length in range(3, n+1): # 3 to 6
+        for start in range(n-length+1):
+            end = start + length - 1
+            if s[start] == s[end] and dp[start+1][end-1]:
+                dp[start][end] = True
+                count += 1
+            else:
+                dp[start][end] = False
+    return count
