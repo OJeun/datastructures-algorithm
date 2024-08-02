102397e08dd4037dc662db81bd333208fb952375
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-08-02 11:00:31 -0700

    Solve matrix chain multiplication using recursive solution and calculate time and space complexity

---
+++ dynamic_programming/matrix_chain_multiplication.py
@@ -0,0 +1,16 @@
+# Space Complexity = O(n)
+# TIme Complexity = O(n * 2^n) 2^n: number of partitions 
+def matrix_chain_multiplication(N, arr):
+    
+    def partitions(start, end):
+        if start == end:
+            return 0
+    
+        min_cost = float('inf')
+        for partition in range(start, end):
+            curr_cost = partitions(1, partition) + partitions(partition+1, end) + arr[start-1] * arr[start] * arr[end]
+            min_cost = min(min_cost, curr_cost)
+
+        return min_cost
+
+    return partitions(1, N-1)
