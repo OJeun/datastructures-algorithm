d2c5a6dfdd87ce73f00a76058df22c4729019d95
Julie Oh <ohjeoung5224@gmail.com>
2025-01-13 19:41:45 +0900

    Separate datastructure and algoritm part

---
+++ Datastructure/linked_list.py
@@ -0,0 +1,141 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp.next is not None:
+            print(temp.value)
+            temp = temp.next
+
+    # O(1)
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    # O(n)
+    def pop(self):
+        if self.head and self.tail:
+            first_pointer = self.head
+            while first_pointer.next:
+                second_pointer = first_pointer
+                first_pointer = first_pointer.next
+            self.tail = second_pointer
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return first_pointer
+        else:
+            return None
+    # O(1)
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    # O(1)
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp
+
+    # O(n)
+    def get(self, index):
+        if index >= self.length + 1 or index < 0:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+        
+    # O(n)
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        else:
+            return False
+            
+    # O(n)
+    def insert(self, index, value):
+        if index > self.length or index < 0:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        pre = self.get(index - 1)
+        new_node.next = pre.next
+        pre.next = new_node
+        self.length += 1
+        return True
+    
+    # O(n)
+    def remove(self, index):
+        if index > self.length - 1 or index < 0:
+            return None
+        if index == self.length - 1:
+            return self.pop()
+        if index == 0:
+            return self.pop_first()
+        pre = self.get(index -1)
+        temp = pre.next
+        pre.next = temp.next
+        return temp
+
+    # O(n)
+    def reverse(self):
+        if self.length == 1:
+            return True
+        if self.length == 0:
+            return False
+        temp = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+            if pre == self.head:
+                self.head.next = None
+            next_temp = temp.next
+            temp.next = pre
+            temp = next_temp
+        return True
+            
+
+
+
+
+
+        
