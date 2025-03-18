d97a79e5ba3557bea2883ab9ee14fd895d83929d
Julie Oh <ohjeoung5224@gmail.com>
2025-03-18 11:20:06 -0700

    Solve Floyd's cycle-finding algorithm

--- Datastructure/linked_list.py
+++ Datastructure/linked_list.py
@@ -145,6 +145,17 @@ class LinkedList:
 
         return slow
 
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
+        
 
 
 
