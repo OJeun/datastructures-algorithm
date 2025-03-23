37a568f57705a7c7d3cd14f97f3901858393506c
Julie Oh <ohjeoung5224@gmail.com>
2025-03-23 16:56:21 -0700

    Find an item in common using hash table

---
+++ Datastructure/hash_table.py
@@ -0,0 +1,48 @@
+class HashTable:
+    # The size of the map should be prime number to increase the randomness of the hash
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    # limit the access and to use name mangling(make it like a private)
+    # _ClassName__functionName
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index]:
+            container = self.data_map[index]
+            for pair in container:
+                if pair[0] == key:
+                    return pair[1]
+            
+        return None
+    
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i]:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+    
+# O(n)
+def item_in_common(list1, list2):
+    my_dict = dict()
+    for item in list1:
+        my_dict[item] = True
+
+    for element in list2:
+        if element in my_dict:
+            return True
+        
+    return False
