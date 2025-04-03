4490c612751ee2ff1928aea999ebde5eb182a7b6
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-03 12:52:17 -0700

    Time: 27 ms (48.09%), Space: 23.2 MB (79.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,17 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         right_product = 1
-        product = [1, nums[0]]
+        product = [1]
 
         # fill product list, if i = 2, proudct[i] = product of elements that are less than index 2.
-        for i in range(2, len(nums)):
-            product.append(product[i-1] * nums[i-1])
-
-
+        for i in range(1, len(nums)):
+            product.append(product[i - 1] * nums[i - 1])
         for j in range(len(nums) - 2, -1, -1):
-            right_product *= nums[j+1]
+            right_product *= nums[j + 1]
             product[j] = product[j] * right_product
 
         return product
-        
-
