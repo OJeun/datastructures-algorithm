df892f1b1a320ac59f45c6aa26f5b5359bfbc1fa
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-03 10:52:00 -0700

    Time: 0 ms (100%), Space: 19.4 MB (52.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,13 +4,12 @@ class Solution:
         count = 0
 
         for num in nums:
+            if count == 0:
+                candidate = num
             if candidate == num:
                 count += 1
 
             else:
                 count -= 1
-                if count == 0:
-                    candidate = num
-                    count += 1
 
         return candidate
