3dc651720664d2100870c7eb1d33506b418a1de7
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-13 16:04:13 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -9,11 +9,12 @@ class Solution:
                     max_water =  max(max_water, 1 * min(height[end], height[start - 1]))
                 else:
                     max_water = max(max_water, (end - start) * min(height[end], height[start]))
+                    if height[start] > height[end]:
+                        end -= 1
+                    else:
+                        start += 1
                 
-                if height[start] > height[end]:
-                    end -= 1
-                else:
-                    start += 1
+
                     
         
         return max_water 
