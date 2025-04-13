fc165a52021d2b3f5d28d427f5147b0f9f85db05
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-13 16:04:49 -0700

    Time: 100 ms (63.88%), Space: 28.3 MB (98.14%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,17 +4,13 @@ class Solution:
         start = 0
         end = len(height) - 1
 
-        while start <= end:
-                if start == end:
-                    max_water =  max(max_water, 1 * min(height[end], height[start - 1]))
-                else:
-                    max_water = max(max_water, (end - start) * min(height[end], height[start]))
-                    if height[start] > height[end]:
-                        end -= 1
-                    else:
-                        start += 1
+        while start < end:
+                max_water = max(max_water, (end - start) * min(height[end], height[start]))
                 
-
+                if height[start] > height[end]:
+                    end -= 1
+                else:
+                    start += 1
                     
         
         return max_water 
