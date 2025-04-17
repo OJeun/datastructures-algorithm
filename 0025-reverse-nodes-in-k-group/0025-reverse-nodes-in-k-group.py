e8086a03396f25645beaee2258903f2576015070
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-17 14:06:29 -0700

    Time: 4 ms (15.03%), Space: 18.7 MB (19.43%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,48 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode()
-        prev = dummy
         dummy.next = head
+        prev = dummy
 
-        curr = head
-        reverse = None
+        def get_kth_node(curr, k):
+            while curr and k > 0:
+                curr = curr.next
+                k -= 1
+            return curr
 
-        while curr:
-            count = k
-            tail = curr
-            reverse = None
+        while True:
+            kth = get_kth_node(prev, k)
+            if not kth:
+                break
 
-            temp = curr
-            for _ in range(k):
-                if temp:
-                    count -= 1
-                    temp = temp.next
-                else:
-                    break
-                
-            
-            if count == 0:
-                for _ in range(k):
-                    next_node = curr.next
-                    curr.next = reverse
-                    reverse = curr
-                    curr = next_node
-                prev.next = reverse
-                prev = tail
-                tail.next = curr
-            else:
-                curr = curr.next if curr else None
+            next_group = kth.next
+            # reverse between prev.next and kth
+            prev_curr = prev.next
+            curr = prev_curr.next
 
+            while curr != next_group:
+                temp = curr.next
+                curr.next = prev.next
+                prev.next = curr
+                curr = temp
 
-
-
-
-
+            prev_curr.next = next_group
+            prev = prev_curr
 
         return dummy.next
-            
