2d494ea09ebb66a1d4707bf55d2fb4a68b0785e5
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-28 16:51:14 -0700

    Time: 0 ms (100%), Space: 17.7 MB (60.89%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,27 +1,23 @@
 import math
 class Solution:
     def isHappy(self, n: int) -> bool:
-        sum_of_squares = n
-        squares_set = set()
-        squares_set.add(n)
+        seen = set()
+        str_n = str(n)
 
         while True:
-            num_digits = int(math.log(sum_of_squares, 10))
-            new_sum = 0
-            while num_digits >= 0:
-                pow_digits = pow(10, num_digits)
-                digit = sum_of_squares // pow_digits
-                new_sum += pow(digit, 2)
-                
-                sum_of_squares %= pow_digits
-                num_digits -= 1
+            square_sum = 0
+            for digit in str_n:
+                square_sum += pow(int(digit), 2)
 
-            sum_of_squares = new_sum 
-            
-            if sum_of_squares == 1:
+            if square_sum == 1:
                 return True
             
-            if sum_of_squares in squares_set:
+            if square_sum in seen:
                 return False
+            
+            seen.add(square_sum)
 
-            squares_set.add(new_sum)
+            str_n = str(square_sum)
+
+
+            
