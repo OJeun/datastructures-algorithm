4c902e9f73d53d9192dd0148f5ce49678e1d4a07
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-27 19:46:10 -0700

    Time: 3 ms (78.37%), Space: 19.5 MB (96.5%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -7,22 +7,20 @@
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
         self.min = float('inf')
-        self.sorted_list = []
+        self.prev = None
 
         def sort_binary_tree(node):
             if not node:
                 return None
             
-            left = sort_binary_tree(node.left)
-            self.sorted_list.append(node.val)
-            right = sort_binary_tree(node.right)
+            sort_binary_tree(node.left)
+            
+            if self.prev:
+                self.min = min(self.min, abs(self.prev.val - node.val))
+            self.prev = node
 
-        def find_minimum_diff(sorted_list):
-            for i in range(len(sorted_list) -1):
-                diff = abs(sorted_list[i] - sorted_list[i+1])
-                self.min = min(diff, self.min)
+            sort_binary_tree(node.right)
+
 
         sort_binary_tree(root)
-        find_minimum_diff(self.sorted_list)
-
         return self.min
