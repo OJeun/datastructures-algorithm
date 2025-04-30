0be9eb635ef32e408630f03393f28c570a865930
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-04-30 10:37:27 -0700

    Time: 3 ms (89.2%), Space: 21.7 MB (61.56%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,18 +3,14 @@ class Solution:
         columns = len(board[0])
         rows = len(board)
         self.board = board
-        self.seen = set()
 
         def dfs(r, c):
-            self.seen.add((r,c))
             self.board[r][c] = "S"
 
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 calc_r, calc_c = r + dr, c + dc
-                if calc_r >= 0 and calc_r < rows and calc_c >= 0 and calc_c < columns:
-                    if self.board[calc_r][calc_c] == "O" and (calc_r, calc_c) not in self.seen:
-                        dfs(calc_r, calc_c)
-
+                if 0 <= calc_r < rows and 0 <= calc_c < columns and board[calc_r][calc_c] == "O":
+                    dfs(calc_r, calc_c)
 
         def updateBoard():
             for row in range(rows):
@@ -23,15 +19,15 @@ class Solution:
                         self.board[row][column] = "X"
                     if self.board[row][column] == "S":
                         self.board[row][column] = "O"
-                        
+
         for row in range(rows):
             for column in range(columns):
                 if (0 < row < rows - 1) and (column == 0 or column == columns - 1):
-                    if self.board[row][column] == "O" and (row, column) not in self.seen:
+                    if self.board[row][column] == "O":
                         dfs(row, column)
 
                 if row == 0 or row == rows - 1:
-                     if self.board[row][column] == "O" and (row, column) not in self.seen:
+                     if self.board[row][column] == "O":
                         dfs(row, column)
 
             
