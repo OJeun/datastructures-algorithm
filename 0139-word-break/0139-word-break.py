481c1842855421f1d1cc6f6c787ae77a55f8297a
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-05-21 09:06:58 -0700

    Time: 7 ms (23.94%), Space: 18.1 MB (24.25%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,21 +1,25 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
         s_length = len(s)
-        word_dict_set = set(wordDict)
-        memo = [None] * s_length  
+        set_word_dict = set(wordDict)
+        memoization = [None] * s_length
 
         def helper(index):
-            if index == s_length:
-                return True
-            if memo[index] is not None:
-                return memo[index]
-
+            if memoization[index] is not None:
+                return memoization[index]
+            
             for end in range(index + 1, s_length + 1):
-                if s[index:end] in word_dict_set and helper(end):
-                    memo[index] = True
-                    return True
+                word = s[index : end]
 
-            memo[index] = False
+                if word in set_word_dict:
+                    if end == s_length:
+                        memoization[index] = True
+                        return True
+                    if helper(end):
+                        memoization[index] = True
+                        return True
+            
+            memoization[index] = False
             return False
 
         return helper(0)
