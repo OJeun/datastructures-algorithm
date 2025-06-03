b9b1a98efd88826364b18ed32bd86f358d8da7fe
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-06-03 11:26:49 -0700

    Time: 48 ms (7.35%), Space: 17.7 MB (56.49%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,21 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        result = ""
-
-        for i in range(31, -1, -1):
-            bit = n >> i & 1
-            result = str(bit) + result
-
-        return self.binaryToInt(result)
-
-
-
-    def binaryToInt(self, n:str) -> int:
         result = 0
 
-        for bit in n:
-            result = result * 2 + int(bit)
+        for i in range(32):
+            bit = n >> i & 1
+            result = (result << 1) | bit
 
         return result
-
-
