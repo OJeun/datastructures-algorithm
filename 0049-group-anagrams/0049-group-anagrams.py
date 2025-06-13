e4938d1a2c4b5ab0ba2df335995cef06cb43e722
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-06-13 10:24:16 -0700

    Time: 11 ms (83.37%), Space: 21 MB (50.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs):
-        anagrams_dict = {}
+        output = []
         index = 0
-        anagrams = []
+
+        anagram_dict = dict()
+
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            sorted_word_index = anagrams_dict.get(sorted_word)
-            if anagrams_dict.get(sorted_word) is None:
-                anagrams_dict[sorted_word] = index
-                anagrams.append([word])
-                index += 1
+            sorted_word = "".join(sorted(word))
+            is_anagram = anagram_dict.get(sorted_word)
+
+            if is_anagram is not None:
+                index_in_output = is_anagram
+                output[index_in_output].append(word)
             else:
-                anagrams[sorted_word_index].append(word)
-        
-        return anagrams
+                anagram_dict[sorted_word] = index
+                output.append([word])
+                index += 1
+            
+            
 
-
+        return output
+            
