ebabbe007af843ad503264fa8602b22ff273581c
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-06-20 15:52:09 -0700

    Time: 0 ms (100%), Space: 18.9 MB (51.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = {val:index for index, val in enumerate(nums)}
+        track = dict()
+        pair = []
+        nums_length = len(nums)
 
-        for i in range(len(nums)):
-            substraction = target - nums[i]
-            index = nums_dict.get(substraction)
-            if index and index > i:
-                return [nums_dict[substraction], i]
+        for index in range(nums_length):
+            num = nums[index]
+            rest = target - num
+            if rest in track:
+                pair.append(track[rest])
+                pair.append(index)
+                return pair
+            else:
+                track[num] = index
+
+        return pair
+            
