1d20c20678f0af33fdd2955074780af1a3a89531
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-06-20 12:07:49 -0700

    Time: 10 ms (75.39%), Space: 17.7 MB (86.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -9,7 +9,7 @@ class Solution:
             s_dict[s_char] = s_dict.get(s_char, 0) + 1
         
         for t_char in t:
-            if t_char in s_dict and s_dict.get(t_char) > 0:
+            if s_dict.get(t_char, 0) > 0:
                 s_dict[t_char] = s_dict[t_char] - 1
             else:
                 return False
