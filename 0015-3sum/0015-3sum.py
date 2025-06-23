e18df8b3fb2c225cf8d0b2dd0a30165fdd66fa33
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-06-23 14:02:11 -0700

    Time: 551 ms (71.76%), Space: 20.7 MB (36.73%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,37 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        n = len(nums)
-        res = []
+        nums_length = len(nums)
+        output = []
 
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i-1]:
+        for curr in range(nums_length - 2):
+            if curr > 0 and nums[curr] == nums[curr - 1]:
                 continue
 
-            first = i + 1
-            second = n - 1
+            left = curr + 1
+            right = nums_length - 1
 
-            while first < second:
-                three_sum =  nums[i] + nums[first] + nums[second]
+            
+
+            while left < right:
+                three_sum = nums[curr] + nums[left] + nums[right]
+
                 if three_sum > 0:
-                    second -= 1
+                    right -= 1
+
                 elif three_sum < 0:
-                    first += 1
-                else: 
-                    res.append([nums[i], nums[first], nums[second]])
-                    first += 1
-                    second -= 1
+                    left += 1
 
-                    while first < second and nums[first] == nums[first - 1]:
-                        first += 1
+                else:
+                    output.append([nums[curr], nums[left], nums[right]])
+                    
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
 
-                    while first < second and nums[second] == nums[second + 1]:
-                        second -= 1    
-        return res
+                    right -= 1
+                    left += 1
+                    
+        return output
