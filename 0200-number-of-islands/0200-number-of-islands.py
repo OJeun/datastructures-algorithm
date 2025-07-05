c4ac22e3104d329eafcccd8ab4156d92d9b21c75
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-07-05 16:47:57 -0700

    Time: 230 ms (90.11%), Space: 20.3 MB (57.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,42 +1,44 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        horizontal = len(grid[0]) # 5
-        vertical = len(grid) # 4
-
-        self.grid = grid
+        # define a variable to count the num of islands
         islands = 0
+        # define two variables length of rows and columns 
+        rows = len(grid)
+        columns = len(grid[0])
+        queue = deque()
 
-                # change 1 to 0 of lands in an island
-        def dfs(v, h):
-            if v < 0 or v >= vertical or h < 0 or h >= horizontal:
-                return
+        # for loop to iterate a grid row
+        for row in range(rows):
+            # iterate columns of a row
+            for column in range(columns):
+                curr_cell = grid[row][column] 
+                # if (row, column) is not visited
+                if curr_cell == '1':
+                    queue.append((row, column))
+                    
+                    # traverse the grid using bfs 
+                    while len(queue) > 0:
+                        r, c = queue.pop()
+                        grid[r][c] = 'v'
+                        # up, (r-1, c)
+                        if r - 1 >= 0 and grid[r-1][c] == "1":
+                            queue.append((r-1, c))
 
-            if self.grid[v][h] == "0":
-                return
-            
-            self.grid[v][h] = "0"
+                        # down (r+1, c)
+                        if r + 1 < rows and grid[r+1][c] == "1":
+                            queue.append((r+1, c))
 
-            up = dfs(v-1, h)
-            down = dfs(v+1, h)
-            right = dfs(v, h+1)
-            left = dfs(v, h-1)
+                        # left (r, c - 1)
+                        if c - 1 >= 0 and grid[r][c - 1] == "1"  :
+                            queue.append((r, c - 1))
 
-        # O(n^2) for loop to iterate 2D array to find a 1(land)
-        for m in range(vertical): 
-            for n in range(horizontal):
-                if self.grid[m][n] == "1":
+                        # right (r, c + 1)
+                        if c + 1 < columns and grid[r][c + 1] == "1":
+                            queue.append((r, c + 1))
+                    
                     islands += 1
-                    dfs(m, n)
 
         return islands
-
-# 핵심 아이디어
-# grid를 한 칸 한 칸 순회한다.
-
-# '1'을 발견하면 → 새로운 섬을 찾은 거다 → 섬 개수 +1
-
-# '1'을 발견했으면, 그 섬에 연결된 모든 '1'들을 DFS로 전부 방문해서 '0'으로 바꾼다. (이미 방문한 것처럼 표시)
-
-# 이렇게 하면 같은 섬을 두 번 세지 않는다.
-
-# = 연결된 모든 '1'을 한번에 지워버리기 (방문 처리)
+                        
+                    
