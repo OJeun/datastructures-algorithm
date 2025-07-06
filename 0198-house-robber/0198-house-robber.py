8568d377107ae769851ced7023f2681a138145b3
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-07-06 13:50:19 -0700

    Time: 0 ms (100%), Space: 17.7 MB (82.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,28 @@
 class Solution:
-    def rob(self, nums: list[int]) -> int:
-        memoization = [-1] * len(nums)
-
-        def helper(n) -> int: 
-            index = n - 1
-
-            if n == 0:
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        # define a helper func(index) => maximum sum up to ith house
+        def helper(index) -> int:
+            # will check maximum at index -1 and index -2 return maximum money
+            if index < 0:
                 return 0
 
-            if n == 1:
-                memoization[index] = nums[index]
-                return nums[index]
-            
-                
-            if memoization[index] != -1:
-                return memoization[index]
-            else:
-                max_rob_to_nth_house = max(helper(n - 1), nums[index] + helper(n - 2))
-                memoization[index] = max_rob_to_nth_house
-                return max_rob_to_nth_house
+            if dp[index] != -1:
+                return dp[index]
 
-        return helper(len(nums))
+            # past_one = helper(index - 1)
+            exclude = helper(index - 1)
+
+            # past_two = helper(index - 2)
+            include = nums[index] + helper(index - 2)
+
+            # maximum_up_to_index = max(past_one, past_two + nums[index])
+            max_up_to_index = max(exclude, include)
+
+            dp[index] = max_up_to_index
+
+            return max_up_to_index
+
+        return helper(len(nums) - 1)
+
+        
