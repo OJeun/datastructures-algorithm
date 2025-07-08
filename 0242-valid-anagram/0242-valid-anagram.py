7ed8e45c0624c66b673bc17797e090705699ef4c
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-07-08 10:02:45 -0700

    Time: 11 ms (72.43%), Space: 17.8 MB (86.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = {}
+        # define a dictionary to store freqeucny of each char in s
+        frequencies = dict()
 
         if len(s) != len(t):
             return False
 
-        for s_char in s:
-            s_dict[s_char] = s_dict.get(s_char, 0) + 1
-        
-        for t_char in t:
-            if s_dict.get(t_char, 0) > 0:
-                s_dict[t_char] = s_dict[t_char] - 1
+        for char in s:
+            frequencies[char] = frequencies.get(char, 0) + 1
+
+        # iterate t using for loop
+        for char in t:
+            # check each char is in dict
+            if char in frequencies and frequencies[char] > 0:
+                # if it does, decrease count by 1
+                frequencies[char] -= 1
+            # else: return False
             else:
                 return False
         
+        # return True
         return True
