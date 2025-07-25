6f80f278857da8018ef08b4176333242886d1ef0
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-07-25 15:00:31 -0700

    Time: 0 ms (100%), Space: 17.9 MB (54.58%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,49 +1,32 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:  
-        def helper(left, right):
-            if not left and not right:
-                return True
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def recursive(left, right):
+            first_sub_tree, second_sub_tree = True, True
 
-            if (left and not right) or (not left and right):
+            if (not left.left and right.right) or (not left.right and right.left):
                 return False
 
-            if left.right and right.left:
-                if left.right.val != right.left.val:
-                    return False
+            if (left.left and not right.right) or (left.right and not right.left):
+                return False
 
             if left.left and right.right:
                 if left.left.val != right.right.val:
                     return False
-
-            return helper(left.right, right.left) and helper(left.left, right.right)
-        
-        if root.left and root.right:
-            if root.left.val == root.right.val:
-                return helper(root.left, root.right)
-            else:
-                return False
-        else:
-            if not root.left and not root.right:
-                return True
-            else:
-                return False
-
-        
-
-
-
-
+                else:
+                    first_sub_tree = recursive(left.left, right.right)
             
+            if left.right and right.left:
+                if left.right.val != right.left.val:
+                    return False
+                else:
+                    second_sub_tree = recursive(right.left, left.right) 
 
+            return first_sub_tree and second_sub_tree
 
+        if not root.left and not root.right:
+            return True
+
+        if root.left and root.right and root.left.val == root.right.val:
+            return recursive(root.left, root.right)
         
-
-        
-            
+        return False
