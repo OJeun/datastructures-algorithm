6ba9872a89b2e9db769ec6d24f632d62427d7471
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-07-25 16:32:51 -0700

    Time: 0 ms (100%), Space: 17.8 MB (54.58%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,32 +1,15 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         def recursive(left, right):
-            first_sub_tree, second_sub_tree = True, True
+            if not left and not right:
+                return True
 
-            if (not left.left and right.right) or (not left.right and right.left):
+            if not left or not right:
                 return False
 
-            if (left.left and not right.right) or (left.right and not right.left):
-                return False
+            return (left.val == right.val
+            and recursive(left.left, right.right)
+            and recursive(right.left, left.right)
+            )
 
-            if left.left and right.right:
-                if left.left.val != right.right.val:
-                    return False
-                else:
-                    first_sub_tree = recursive(left.left, right.right)
-            
-            if left.right and right.left:
-                if left.right.val != right.left.val:
-                    return False
-                else:
-                    second_sub_tree = recursive(right.left, left.right) 
-
-            return first_sub_tree and second_sub_tree
-
-        if not root.left and not root.right:
-            return True
-
-        if root.left and root.right and root.left.val == root.right.val:
-            return recursive(root.left, root.right)
-        
-        return False
+        return recursive(root.left, root.right)
