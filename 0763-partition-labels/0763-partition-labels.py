6766078dc135fd879866fbe7e6daf1612f81df54
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-07-28 15:23:06 -0700

    Time: 5 ms (53.75%), Space: 17.8 MB (54.55%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,28 +1,29 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        char_index = [-1] * 26
+        char_index = [-1] * 26 # index = char position in alphabets (a = 0, b= 1) # value = last index of that character in s
         output = []
-        partition = 0
-        start = 0
 
-        for position, char in enumerate(s):
-            index = ord(char) - ord('a')
-            char_index[index] = position
+        partition_end = 0
+        partition_start = 0
+
+        for index, char in enumerate(s):
+            position = ord(char) - ord('a')
+            char_index[position] = index
 
         for index, char in enumerate(s): 
-            if partition == 0:
-                start = index
+            if partition_end == 0:
+                partition_start = index
 
             position = ord(char) - ord('a')
-            max_index = char_index[position]
+            last_index = char_index[position]
 
-            if index <= max_index:
-                partition = max(partition, max_index)
+            if index <= last_index:
+                partition_end = max(partition_end, last_index)
 
-            if index == partition:
-                partition_length = partition - start + 1
+            if index == partition_end:
+                partition_length = partition_end - partition_start + 1
                 output.append(partition_length)
-                partition = 0
+                partition_end = 0
 
         return output
 
