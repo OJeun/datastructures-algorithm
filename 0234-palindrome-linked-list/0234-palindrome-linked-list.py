c219ef2b6e18491312d3d4b86c8b108c9fa6f55f
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-08-12 09:19:17 -0700

    Time: 46 ms (22.18%), Space: 34.8 MB (84.15%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,40 +1,37 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # define two pointers slow and fast initializing it to head
-        slow, fast = head, head
+        fast = head
+        slow = None
 
-        # while loop until fast reaches to the end of the list
-        while fast and fast.next and fast.next.next:
-            # move fast two steps, slow on step at once
+        while fast and fast.next:
             fast = fast.next.next
-            slow = slow.next
 
-        # make a new pointer(new_head) that points next node of slow and then slow.next points to None
-        curr = slow.next
-        slow.next = None
+            if slow is None:
+                slow = head
+            else:
+                slow = slow.next
 
         temp = None
-        # create curr pointer initiazliing it to new_head
-        
-        # while loop until curr exists => reverse the second half
+        prev = slow
+        curr = slow.next if slow else None
+    
         while curr:
-            # prepend the current node to new_head
             next_node = curr.next
+
             curr.next = temp
             temp = curr
             curr = next_node
 
-        # compare the first half with reversed second half
-        while temp and head:
-            # if they have different value
-            if temp.val != head.val:
+        left = head
+        right = temp
+
+        while right and left:
+            if right.val != left.val:
                 return False
-            temp = temp.next
-            head = head.next
+            right = right.next
+            left = left.next
 
         return True
+
+
+        
