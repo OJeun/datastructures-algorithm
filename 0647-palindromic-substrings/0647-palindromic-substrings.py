d394ab28506e7fd20090407a66caf24433b52bad
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-08-14 12:01:58 -0700

    Time: 126 ms (78.96%), Space: 18 MB (34.98%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,32 +1,31 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        dp = []
-        self.count = 0
-        for _ in range(len(s)):
-            dp.append([-1] * len(s))
+        count = 0
+        
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                
+                l -= 1
+                r += 1
+
+
+        for i in range(len(s) - 1):
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+
+        return count 
+
+
+
         
 
-        def recursive(start, end) -> bool:
-            if start >= end:
-                return True
-
-            if dp[start][end] != -1:
-                return dp[start][end]
-
-            if s[start] == s[end] and recursive(start+1, end-1):
-                dp[start][end] = True
-                self.count += 1
-                return True
-
-            dp[start][end] = False
-                
-
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                if dp[i][j] == -1:
-                    recursive(i, j)
-
-        return self.count + len(s)
 
 
+
+        
         
