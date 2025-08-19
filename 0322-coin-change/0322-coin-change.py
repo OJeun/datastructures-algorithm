7c8a503ba34d803979809eb9b88c6b1d3c6e5db6
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-08-19 11:10:46 -0700

    Time: 689 ms (86.53%), Space: 18.1 MB (68.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,32 +1,56 @@
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
-        memoization = [None] * (amount + 1)
-        
-        def helper(rest_amount):
-            minimum = float('inf')
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        INF = amount + 1
 
-            if rest_amount == 0:
-                return 0
-
-            if rest_amount < 0:
-                return -1 
-
-            if memoization[rest_amount] is not None:
-                return memoization[rest_amount]
-
+        for i in range(1, amount + 1):
+            min_count = INF
             for coin in coins:
-                number_of_coin = helper(rest_amount - coin)
+                rem = i - coin
+                if rem >= 0 and dp[rem] != INF:
+                    min_count = min(dp[rem] + 1, min_count)
 
-                if number_of_coin != -1:
-                    minimum = min(minimum, number_of_coin + 1)
+            dp[i] = min_count
+        
+        ans = dp[amount]
+        return ans if ans is not INF else -1
 
-            if minimum != float('inf'):
-                memoization[rest_amount] = minimum
-                return minimum
-            else:
-                memoization[rest_amount] = -1
-                return -1
-            
                 
-        return helper(amount)
+                    
 
+
+
+
+
+
+
+
+
+
+
+
+
+    def coinChange_bottom_up_recursive(self, coins: list[int], amount: int) -> int:
+        INF = amount + 1
+        dp = [-1] * (amount + 1)
+
+        def dfs(rem):
+            if rem < 0:
+                return INF
+
+            if rem == 0:
+                return 0
+            
+            if dp[rem] != -1:
+                return dp[rem]
+            
+            min_count = INF
+            for coin in coins:
+                min_count = min(min_count, dfs(rem - coin) + 1)
+
+            dp[rem] = min_count
+            return min_count
+
+        ans = dfs(amount)
+        return -1 if ans >= INF else ans
