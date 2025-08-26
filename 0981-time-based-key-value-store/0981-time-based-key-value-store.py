223d90af33232d609c714703bc5d08aa23fab420
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-08-26 15:02:16 -0700

    Time: 162 ms (17.38%), Space: 70.8 MB (98.53%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -10,35 +10,23 @@ class TimeMap:
         self.values[key].append(value)
 
     def get(self, key: str, timestamp: int) -> str:
-        if not self.timestamps[key] or timestamp < self.timestamps[key][0]:
-            return ""
-
         index = self._binarySearch(0, len(self.values[key]) -1, self.timestamps[key], timestamp)
 
         return "" if index == -1 else self.values[key][index]
 
-    def _binarySearch(self, start, end, timestamps, target):
+    def _binarySearch(self, start, end, timestamps, target, ans=-1):
         mid = (start + end) // 2
         if start > end:
-            return -1
-
-        if start == end:
-            return start
-        
-        if start + 1 == end:
-            if target < timestamps[end]:
-                return start
-            else:
-                return end
+            return ans
 
         if target == timestamps[mid]:
             return mid
 
         if target < timestamps[mid]:
-            return self._binarySearch(start, mid - 1, timestamps, target)
+            return self._binarySearch(start, mid - 1, timestamps, target, ans)
 
         if target > timestamps[mid]:
-            return self._binarySearch(mid, end, timestamps, target)
+            return self._binarySearch(mid + 1, end, timestamps, target, mid)
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
