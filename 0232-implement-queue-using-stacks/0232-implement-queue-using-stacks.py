f74fc59ec5198b88c4050d1eded1eb33ea8f4b48
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-09-03 09:30:23 -0700

    Time: 0 ms (100%), Space: 18 MB (38.96%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,18 +1,15 @@
 class MyQueue:
-
     def __init__(self):
         self.stack = []
         self.reversed_stack = []
-        
 
     def push(self, x: int) -> None:
         self.stack.append(x)
 
     def pop(self) -> int:
         if not self.reversed_stack:
-            while self.stack:
-                item = self.stack.pop()
-                self.reversed_stack.append(item)
+            for _ in range(len(self.stack)):
+                self.reversed_stack.append(self.stack.pop())
 
         return self.reversed_stack.pop()
 
@@ -21,13 +18,14 @@ class MyQueue:
             return self.reversed_stack[-1]
         else:
             return self.stack[0]
-        
 
     def empty(self) -> bool:
-        if self.reversed_stack or self.stack:
+        if self.stack or self.reversed_stack:
             return False
         else:
             return True
+        
+        
 
 
 # Your MyQueue object will be instantiated and called as such:
