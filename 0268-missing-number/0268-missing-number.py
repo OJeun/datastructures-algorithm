50fbea68cccb23815549292241afafc55d56b181
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-09-03 10:36:13 -0700

    Time: 7 ms (36.6%), Space: 18.7 MB (51.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,9 +4,6 @@ class Solution:
         total = 0
 
         for i in range(1, n + 1):
-            total += i
-
-        for num in nums:
-            total -= num
+            total += (i - nums[i-1])
 
         return total
