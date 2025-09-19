583387c02d65b70884d828242a0f50d01deec982
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-09-19 10:09:03 -0700

    Time: 0 ms (100%), Space: 18.2 MB (26.64%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,34 +2,27 @@ from collections import deque
 
 class Solution:
     def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
+        rows = len(image)
+        columns = len(image[0])
         original_color = image[sr][sc]
-        # mark = []
 
-        # for _ in range(len(image)):
-        #     mark.append(["x"] * len(image[0]))
+        if original_color == color:
+            return image
 
-        queue = deque()
-        queue.append((sr, sc))
-        # mark[sr][sc] = "v"
-        
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        # bfs
-        while queue:
-            sr, sc = queue.popleft()
-            
-            # update current colour to color if curr colour is the same as original
-            if image[sr][sc] == original_color:
-                image[sr][sc] = color
+        
+        # dfs
+        def dfs(curr_r, curr_c):
 
             for r, c in directions:
-                row = sr + r
-                column = sc + c
-                
+                row = curr_r + r
+                column = curr_c + c
 
-                if 0 <= row < len(image) and 0 <= column < len(image[0]):
-                    curr_color = image[row][column]
-                    if curr_color != color and curr_color == original_color:
-                        # mark[row][column] = "v"
-                        queue.append((row, column))
-
+                if 0 <= row < rows and 0 <= column < columns:
+                    if image[row][column] == original_color:
+                        image[row][column] = color
+                        dfs(row, column)
+                        
+        image[sr][sc] = color
+        dfs(sr, sc)
         return image
