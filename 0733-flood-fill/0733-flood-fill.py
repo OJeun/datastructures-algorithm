ff8c3b458e79495dc4890a6671bd06f6976a670e
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-09-19 09:31:01 -0700

    Time: 0 ms (100%), Space: 17.8 MB (88.31%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
+        original_color = image[sr][sc]
+        mark = []
+
+        for _ in range(len(image)):
+            mark.append(["x"] * len(image[0]))
+
+        queue = deque()
+        queue.append((sr, sc))
+        mark[sr][sc] = "v"
+        
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        # bfs
+        while queue:
+            sr, sc = queue.popleft()
+            
+            # update current colour to color if curr colour is the same as original
+            if image[sr][sc] == original_color:
+                image[sr][sc] = color
+
+            for r, c in directions:
+                row = sr + r
+                column = sc + c
+
+                if 0 <= row < len(image) and 0 <= column < len(image[0]):
+                    if mark[row][column] == "x" and image[row][column] == original_color:
+                        mark[row][column] = "v"
+                        queue.append((row, column))
+
+        return image
