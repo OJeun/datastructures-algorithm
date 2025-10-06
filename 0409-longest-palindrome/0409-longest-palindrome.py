93a97c212063c9a3bf45b39fda59678a3003a8e9
Julie Oh <108641751+OJeun@users.noreply.github.com>
2025-10-06 08:57:14 -0700

    Time: 1 ms (55.87%), Space: 17.8 MB (45.75%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -2,19 +2,23 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         count = 0
         frequency = dict()
-        is_single = False
+        odd_freq_chars = 0
 
         for char in s:
             frequency[char] = frequency.get(char, 0) + 1
 
         for char, freq in frequency.items():
             pair = freq // 2
+            remainder = freq % 2
+
             if pair > 0:
                 count += (pair * 2)
 
-            if freq % 2 == 1 and not is_single:
-                count += 1
-                is_single = True 
+            if remainder == 1:
+                odd_freq_chars += 1
 
+        if odd_freq_chars:
+            count += 1
+            
         return count
     
